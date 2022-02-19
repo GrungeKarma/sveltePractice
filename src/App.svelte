037d0638c508d2eaa3d41b691adb5a4d9a931dae
Website Admin <coordinator@fullmoonprod.com>
2022-02-19 13:45:58 -0600

    first lesson

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let name = "Anthony";
+	let age = 31;
+	const incrementAge = () => {
+		age += 1;
+	};
+</script>
+
+<h1>Hello I am {name}, my age is {age}!</h1>
+<button on:click={incrementAge}>Increase Age</button>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
