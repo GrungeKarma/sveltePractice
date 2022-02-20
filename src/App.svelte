e4602b69c4fb8f760033d832dc63810be97c4919
Website Admin <coordinator@fullmoonprod.com>
2022-02-19 20:01:18 -0600

    binding to element properties

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,32 @@
 <script>
-	let name = "Anthony";
+	let name = "Tony";
 	let age = 31;
+	//let upperCaseName; not required!
+	$: upperCaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === "Anthony") {
+		age = 55;
+	}
 	const incrementAge = () => {
 		age += 1;
 	};
+	const changeName = () => {
+		name = "Anthony";
+	};
+	let nameInput = (event) => {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	};
 </script>
 
-<h1>Hello I am {name}, my age is {age}!</h1>
+<h1>Hello I am {upperCaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Increase Age</button>
+
+<!-- <button on:click={changeName}>Change Name</button> -->
+<input type="text" value={name} on:input={nameInput} />
 
 <style>
 	h1 {
-		color: purple;
+		color: slateblue;
 	}
 </style>
